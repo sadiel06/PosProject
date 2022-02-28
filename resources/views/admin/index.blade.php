729cc9233c1d1f09a214488940dd8ad68b2c49fc
Sadiel Henriguez <sadiel.h.o@gmail.com>
2022-02-28 11:36:59 -0400

    2 graficos chartjs

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,7 +8,15 @@
 @stop
 
 @section('content')
-    <canvas id="myChart"></canvas>
+<div class="row" >
+    <div class="card col m-lg-3" style="max-width: 600px">
+        <canvas style="max-height: 900px; max-width: 600px" class="" id="myChart"></canvas>
+    </div>
+
+    <div class="card col m-lg-3" style="max-width: 600px">
+        <canvas style="max-height: 900px; max-width: 600px" class="" id="yourChart"></canvas>
+    </div>
+</div>
 @stop
 
 @section('css')
@@ -53,7 +61,50 @@
                 }
             }
         });
-        myChart.canvas.parentNode.style.height = '848px';
-        myChart.canvas.parentNode.style.width = '828px';
+        myChart.canvas.parentNode.style.height = 'auto';
+        myChart.canvas.parentNode.style.width = 'auto';
+
+        //otro char
+
+
+        const ctx1 = document.getElementById('yourChart').getContext('2d');
+        const yourChart = new Chart(ctx1, {
+            type: 'polarArea',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 7, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        yourChart.canvas.parentNode.style.height = 'auto';
+        yourChart.canvas.parentNode.style.width = 'auto';
+
     </script>
 @stop
